29a319406e5ccf7a1a749e2fb7aa18287c71bbed
gratien <gracian2020@gmail.com>
2022-07-19 11:25:58 +0200

    setup apollo client

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,18 +2,22 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import '@/utils/Firebase';
 import { Toaster } from 'react-hot-toast';
+import apolloClient from '@/utils/ApolloClient';
+import { ApolloProvider } from '@apollo/client';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Component {...pageProps} />
-      <Toaster
-        toastOptions={{
-          style: { fontSize: '14px', borderRadius: '5px', padding: '15px' },
-          error: { style: { background: '#FF001F', color: 'white' } },
-          success: { style: { background: '#B3FF77', color: 'black' } },
-        }}
-      />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+        <Toaster
+          toastOptions={{
+            style: { fontSize: '14px', borderRadius: '5px', padding: '15px' },
+            error: { style: { background: '#FF001F', color: 'white' } },
+            success: { style: { background: '#B3FF77', color: 'black' } },
+          }}
+        />
+      </ApolloProvider>
     </>
   );
 };
