064977c8b19da1fc48b12485e55d0ec265488e15
gratien <gracian2020@gmail.com>
2022-08-30 17:45:23 +0200

    added setup button in layout

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -4,8 +4,12 @@ import Header from '@/components/Shared/Header/Header';
 import GoBack from './Shared/GoBack';
 import { useRouter } from 'next/router';
 import style from './Style.module.scss';
+import classname from 'classnames';
+import { Icon } from '@iconify/react';
 
 export const Layout = ({ children, goBack }: TLayoutProps) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
   const router = useRouter();
 
   return (
@@ -17,7 +21,18 @@ export const Layout = ({ children, goBack }: TLayoutProps) => {
             <GoBack handleClick={() => router.back()} />
           </div>
         )}
-        {children}
+
+        <> {children}</>
+        <div className={classname('fixed bottom-0', style.setup)}>
+          <button
+            type="button"
+            className={classname(
+              'relative outline-none focus:outline-none bg-primary rounded-full flex items-center justify-center'
+            )}
+          >
+            <Icon icon="akar-icons:plus" fontSize={35} />
+          </button>
+        </div>
       </main>
     </>
   );
