3c867ced4f5018d7b41206a1cb2e53ce3bcebfb0
gratien <gracian2020@gmail.com>
2022-09-18 14:04:47 +0200

    designed page to allow candidates to start quiz

---
+++ web/src/components/Quiz/StartQuiz/Svg.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './StartQuiz.module.scss';
+
+const Svg = () => {
+  return (
+    <svg
+      width="1512"
+      height="526"
+      viewBox="0 0 1512 526"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={style.svg}
+    >
+      <path
+        d="M0 0H1522.98C1522.98 0 1516.94 293.731 1522.98 454.533C1529.03 615.334 0 454.533 0 454.533V0Z"
+        fill="#FFEC44"
+      />
+    </svg>
+  );
+};
+export default Svg;
