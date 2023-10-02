b360968cf68b32c0613bc641119ed2215389a191
Gratien TUYISHIMIRE <gtuyishimire@BKTH-DEV30.bktechouse.net>
2023-10-02 19:04:43 +0200

    fixed send email

--- src/server/Helpers/MailHelper.ts
+++ src/server/Helpers/MailHelper.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import axios from 'axios';
 
 const sender = 'gracian2020@gmail.com';
 const transporter = nodemailer.createTransport({
@@ -8,6 +9,20 @@ const transporter = nodemailer.createTransport({
     pass: 'aakxeoncqztvwcag',
   },
 });
+
+const authenticate = async () => {
+  try {
+    const uri = process.env.COMMUNICATION_SERVICE_URI + '/v1/applications/auth';
+    const res = await axios.post(uri, {
+      app_id: process.env.COMMUNICATION_SERVICE_APP_ID ?? '',
+      access_key: process.env.COMMUNICATION_SERVICE_ACCESS_KEY ?? '',
+    });
+
+    return res.data.data.token;
+  } catch (error) {
+    return undefined;
+  }
+};
 
 export const sendEmail = async ({
   subject,
@@ -20,12 +35,30 @@ export const sendEmail = async ({
   html?: string;
   receivers?: string[];
 }) => {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: sender, // sender address
-    to: receivers, // list of receivers
-    subject, // Subject line
-    text, // plain text body
-    html, // html body
-  });
+  try {
+    const token = await authenticate();
+
+    const uri =
+      process.env.COMMUNICATION_SERVICE_URI + '/v1/messages/sendEmail';
+
+    const res = await axios.post(
+      uri,
+      {
+        recipients: receivers,
+        subject,
+        html,
+        text,
+      },
+      {
+        headers: {
+          'x-access-key': token,
+        },
+      },
+    );
+
+    return res.data;
+  } catch (error: any) {
+    console.log('error:', error);
+    throw new Error(error);
+  }
 };
